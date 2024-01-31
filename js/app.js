6b5b53abb454ff4ef0e5ea092021b844d536c48a
Nodirbek123528 <nodirbek.08@icloud.com>
2024-01-31 19:55:49 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -4,28 +4,44 @@ const headerMenu = document.querySelector('.header-menu')
 const newIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1)"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>`
 const oldIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1)"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>`
 
-for (let item of headerLink){
+
+function closeAllDropdowns(){
+    for(let el of headerLink){
+        el.classList.remove('active');            
+        el.nextElementSibling.classList.remove('active');            
+    }
+}
+
+
+function headerMenuOpen(){
+    headerButtonMenu.classList.add('active')
+    headerMenu.classList.add('active')
+    headerButtonMenu.innerHTML = newIcon
+}
+
+function headerMenuClose(){
+    headerButtonMenu.classList.remove('active')
+    headerMenu.classList.remove('active')
+    headerButtonMenu.innerHTML = oldIcon
+}
+
+for(let item of headerLink){
     item.addEventListener('click', function(){
         let dropdown = item.nextElementSibling
-        let dropdownBg = dropdown.querySelector('.dropdown-bg')
-        
+        let dropdownBg = dropdown.querySelector('.dropdown-bg');
         if(item.classList.contains('active')){
-            item.classList.remove('active')
-            dropdown.classList.remove('active')
+            item.classList.remove('active');
+            dropdown.classList.remove('active');
+        }else{
+            closeAllDropdowns()
+            headerMenuClose()
+            item.classList.add('active');
+            dropdown.classList.add('active');
         }
-        else{
-            for(let el of headerLink){
-                el.classList.remove('active')
-                el.nextElementSibling.classList.remove('active')
-            }
-            headerButtonMenu.classList.remove('active')
-            headerMenu.classList.remove('active')
-            item.classList.add('active')
-            dropdown.classList.add('active')
-        }
+
         dropdownBg.addEventListener('click', function(){
-            item.classList.remove('active')
-            dropdown.classList.remove('active')
+            item.classList.remove('active');
+            dropdown.classList.remove('active');
         })
     })
 }
@@ -33,16 +49,9 @@ for (let item of headerLink){
 
 headerButtonMenu.addEventListener('click', function(){
     if(headerButtonMenu.classList.contains('active')){
-        headerButtonMenu.classList.remove('active')
-        headerMenu.classList.remove('active')
-        headerButtonMenu.innerHTML = oldIcon
+        headerMenuClose()
     }else{
-        for(let el of headerLink){
-            el.classList.remove('active')
-            el.nextElementSibling.classList.remove('active')
-        }
-        headerButtonMenu.classList.add('active')
-        headerMenu.classList.add('active')
-        headerButtonMenu.innerHTML = newIcon
+        closeAllDropdowns()
+        headerMenuOpen()
     }
 })
